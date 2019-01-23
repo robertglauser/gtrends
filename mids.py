d5b75b0d82a4ada90bce1bd9335c63c964356703
popaj <popaj@users.noreply.github.com>
2019-01-23 23:03:22 +0100

    - refactor mid output (#2)
    - remove unused variable

--- mids.py
+++ mids.py
@@ -9,18 +9,14 @@ os.chdir(path)
 
 
 def create_mid_from_keyword(keyword):
-    save_mid_results = 'mids/midresults.csv'
-    print(save_mid_results)
-
-    print("\n\n\n")
     pytrend = TrendReq()
 
     # getMid
     mid_of_keyword = pd.DataFrame(pytrend.suggestions(keyword[0]))
 
-    print("---------------- MID >>> ---------------")
+    print("---------------- AVAILABLE MID >>> ---------------")
     print(mid_of_keyword)
-    print("---------------- <<< MID ---------------\n\n\n")
+    print("-------------------------------\n")
 
     if not mid_of_keyword.empty:
         mids_of_desired_types = mid_of_keyword[(mid_of_keyword['type'].str.lower().str.contains('bank')) | (mid_of_keyword['type'].str.lower().str.contains('company')) | (mid_of_keyword['type'].str.lower().str.contains('corporation'))]
@@ -28,15 +24,12 @@ def create_mid_from_keyword(keyword):
             mid_of_interest = [mids_of_desired_types['mid'].values[0], mids_of_desired_types['title'].values[0], mids_of_desired_types['type'].values[0]]
 
             print("---------------- MID OF INTEREST >>> ---------------")
-            print(mid_of_interest[0])
-            print(mid_of_interest[1])
-            print(mid_of_interest[2])
+            print(mid_of_interest)
             print("---------------- <<< MID OF INTEREST  ---------------\n\n\n")
 
             with open("results.csv", "a") as resultsFile:
                 mid_entry = keyword[0] + ',' + mid_of_interest[0] + ',' + mid_of_interest[1] + ',' + mid_of_interest[2] + "\n"
                 resultsFile.write(mid_entry)
-
 
 
 with open('keywords.csv', 'r') as keywordFile:
